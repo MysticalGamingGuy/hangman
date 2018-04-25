cb40d9ad46867329b2fe0f00e2d5171ea55664bc
Ian Moody <imoody1@hotmail.com>
2018-04-25 10:35:12 -0600

    Images Loaded and User input accepted

--- hangman.js
+++ hangman.js
@@ -1,10 +1,22 @@
 
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+var imgs = [];
+var counter = 0;
+var letters = [];
+
+for(var i = 0; i < 11; i++){
+    var img = new Image();
+    img.src = 'img/hang'+i+'.gif';
+    imgs.push(img);
+}
+
 window.onload = function() {
-    var img_1 = "./green.png";
-    var c=document.getElementById("myCanvas");
-    var ctx=c.getContext("2d");
-    var img=document.getElementById("scream");
-    ctx.drawImage(img,10,10);
+    ctx.drawImage(imgs[counter],10,10);
 };
 
-
+function guess(){
+    var letter = document.getElementById('letter').value;
+    letters.push(letter);
+    console.log(letters);
+}
