bd0ad765808c6513bbf0e54a02ff5968a2a24d0c
Ian Moody <imoody1@hotmail.com>
2018-04-25 11:27:51 -0600

    Guessing Logic Added, Drawing secret word and Incorrect Letters

--- hangman.js
+++ hangman.js
@@ -1,42 +1,96 @@
-
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+var canvas = document.getElementById("myCanvas");
+var context = canvas.getContext("2d");
 var counter = 0;
 var letters = [];
 var secretWord = 'bob';
+var maxGuesses = 10;
+var currentDraw = drawNormal;
 
 var images = [];
-for(var i = 0; i < 11; i++){
+for (var i = 0; i <= maxGuesses; i++) {
     var img = new Image();
-    img.src = 'img/hang'+i+'.gif';
+    img.src = 'img/hang' + i + '.gif';
     images.push(img);
 }
 
 window.addEventListener('resize', fit);
 window.onload = fit();
 
-function fit(){
-    ctx.canvas.width  = window.innerWidth;
-    ctx.canvas.height = window.innerHeight/2;
-    draw()
+function fit() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight / 2;
+    currentDraw()
 }
 
-function guess(){
+function guess() {
     var field = document.getElementById('letter');
-    var letter = field.value;
+    var letter = (field.value).charAt(0);
     field.value = '';
-    letters.push(letter);
-    draw();
+
+    //Make sure the letter hasn't already been guessed
+    if (!letters.includes(letter)) {
+        checkOverLimit(letter);
+        letters.push(letter);
+        checkWin();
+        currentDraw();
+    }
 }
 
-function draw(){
-    ctx.font="60px Georgia";
-    ctx.drawImage(images[counter],10,10);
+function checkOverLimit(letter) {
+    if (!secretWord.includes(letter)) {
+        counter++;
+        if (counter >= maxGuesses) {
+            currentDraw = drawLose;
+            document.getElementById('interact').style.display = 'none';
+        }
+    }
+}
 
-    for(var n = 0; n < secretWord.length; n++)
-            ctx.fillText('_',n*50,200);
+function checkWin() {
+    var winning = true;
+    for (var n = 0; n < secretWord.length; n++)
+        if(!letters.includes(secretWord.charAt(n)))
+            winning = false;
+    if(winning){
+        document.getElementById('interact').style.display = 'none';
+        currentDraw = drawWin;
+    }
+}
 
-    for(var i = 0; i < 11; i++)
-        if(letters[i] != undefined)
-            ctx.fillText(letters[i],i*50,300);
+//The normal state of drawing
+function drawNormal() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.font = "60px Georgia";
+    context.drawImage(images[counter], 10, 10);
+    drawSecretWord();
+    drawIncorrectLetters();
+}
+
+//What to drawNormal when the player loses
+function drawLose() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(images[counter], 10, 10);
+    context.fillText('You Lose', 50, 300);
+    drawSecretWord();
+}
+
+//What to drawNormal when the player Wins
+function drawWin() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.drawImage(images[counter], 10, 10);
+    context.fillText('You Win', 50, 300);
+    drawSecretWord();
+}
+
+//Draw Parts of Secret Words
+function drawSecretWord() {
+    for (var n = 0; n < secretWord.length; n++)
+        context.fillText( letters.includes(secretWord.charAt(n))? secretWord.charAt(n): '_', n * 50, 200);
+}
+
+//Draw Incorrect Letters
+function drawIncorrectLetters() {
+    for (var i = 0; i < 11; i++)
+        if (letters[i] != undefined && !secretWord.includes(letters[i]))
+            context.fillText(letters[i], i * 50, 300);
 }
