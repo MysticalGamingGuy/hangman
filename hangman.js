cf16da2b3b7d4e2ac9c543154a212d6a9ace5981
Ian Moody <imoody1@hotmail.com>
2018-04-25 10:17:55 -0600

    Init Hangman Project

---
+++ hangman.js
@@ -0,0 +1,10 @@
+
+window.onload = function() {
+    var img_1 = "./green.png";
+    var c=document.getElementById("myCanvas");
+    var ctx=c.getContext("2d");
+    var img=document.getElementById("scream");
+    ctx.drawImage(img,10,10);
+};
+
+
